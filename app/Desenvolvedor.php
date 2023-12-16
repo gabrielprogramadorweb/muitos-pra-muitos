9b110f405c71d249adeb0eaf0261a595eeb53956
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-15 22:49:32 -0300

    att

--- app/Desenvolvedor.php
+++ app/Desenvolvedor.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Desenvolvedor extends Model
 {   
-    //usado para renomear nome da tabela
+    // Usado para renomear o nome da tabela no banco de dados
     protected $table = 'desenvolvedores';
+
+    // Define um relacionamento muitos para muitos com o modelo Projeto
+    function projetos(){
+        // Retorna uma instância de BelongsToMany
+        // - O primeiro parâmetro é a classe do modelo relacionado (App\Projeto)
+        // - O segundo parâmetro é o nome da tabela intermediária que armazena as alocações (alocacoes)
+        return $this->belongsToMany("App\Projeto", "alocacoes");
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,23 @@
 use Illuminate\Support\Facades\Route;
 use App\Desenvolvedor;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Aqui é onde você pode registrar rotas da web para sua aplicação. Essas
+| rotas são carregadas pelo RouteServiceProvider dentro de um grupo que
+| contém o grupo de middleware "web". Agora crie algo incrível!
 |
 */
 
+// Define uma rota para acessar desenvolvedores com seus projetos
 Route::get('/desenvolvedor_projeto', function () {
+    
+    // Obtém todos os desenvolvedores com relacionamento "projetos" pré-carregado
+    $desenvolvedores = Desenvolvedor::with('projetos')->get();
 
-    $desenvolvedores = Desenvolvedor::all();
+    // Converte a coleção de desenvolvedores para JSON e retorna
     return $desenvolvedores->toJson();
 });
