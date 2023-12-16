a593df8db0006c0353a7ec8b36069633d4941cf3
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-15 22:39:35 -0300

    att

--- app/Desenvolvedor.php
+++ app/Desenvolvedor.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Desenvolvedor extends Model
-{
-    //
+{   
+    //usado para renomear nome da tabela
+    protected $table = 'desenvolvedores';
 }
