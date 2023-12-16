e0a8cafe1a77b2dd3000c53f9c71f480d1c321d0
Gabriel Castro <gabriel.programador.ads@gmail.com>
2023-12-15 23:29:32 -0300

    att

--- routes/web.php
+++ routes/web.php
@@ -45,14 +45,22 @@ Route::get('/desenvolvedor_projetos', function () {
 
 // Define uma rota para acessar projetos com seus desenvolvedores relacionados
 Route::get('/projeto_desenvolvedores', function () {
+    // Carrega todos os projetos com seus desenvolvedores relacionados
     $projetos = Projeto::with("desenvolvedores")->get();
+
+    // Itera sobre cada projeto
     foreach($projetos as $p) {
+        // Exibe informações do projeto
         echo "Nome do projeto: " . $p->nome . "<br>";
         echo "Estimativa de horas: " . $p->estimativa_horas . "<br>";
-        if (count($p->desenvolvedores ) > 0) {
+
+        // Verifica se o projeto possui desenvolvedores
+        if (count($p->desenvolvedores) > 0) {
+            // Exibe a lista de desenvolvedores do projeto
             echo "Desenvolvedores: <br>";
             echo "<ul>";
             foreach($p->desenvolvedores as $d) {
+                // Exibe informações de cada desenvolvedor relacionado ao projeto
                 echo "<li> Nome do desenvolvedor: " . $d->nome . " | ";
                 echo "Cargo: " . $d->cargo . " | ";
                 echo "Horas trabalhadas pelo desenvolvedor: " . $d->pivot->horas_semanais . "</li>";
@@ -64,16 +72,11 @@ Route::get('/projeto_desenvolvedores', function () {
     //return $projetos->toJson();
 });
 
-
+// Define uma rota para alocar desenvolvedores a um projeto
 Route::get('/alocar', function () {
     $proj = Projeto::find(4);
     if (isset($proj)) {
-        // $proj->desenvolvedores()->attach(1, ['horas_semanais' => 50]);
-        // $proj->desenvolvedores()->attach(2, ['horas_semanais' => 50]);
-        // $proj->desenvolvedores()->attach(3, ['horas_semanais' => 50]);
-        
-        // ou
-
+        // Associa desenvolvedores ao projeto com horas semanais especificadas
         $proj->desenvolvedores()->attach([
             1 => ['horas_semanais' => 40],
             2 => ['horas_semanais' => 50],
@@ -81,17 +84,16 @@ Route::get('/alocar', function () {
         ]);
         return "OK";
     }
-    return "Projeto nao encontrado";
+    return "Projeto não encontrado";
 });
 
-
+// Define uma rota para desalocar desenvolvedores de um projeto
 Route::get('/desalocar', function () {
     $proj = Projeto::find(4);
     if (isset($proj)) {
-        $proj->desenvolvedores()->detach(1);
-        $proj->desenvolvedores()->detach(2);
-        $proj->desenvolvedores()->detach(3);
+        // Desassocia desenvolvedores do projeto
+        $proj->desenvolvedores()->detach([1, 2, 3]);
         return "OK";
     }
-    return "Projeto nao encontrado";
+    return "Projeto não encontrado";
 });
